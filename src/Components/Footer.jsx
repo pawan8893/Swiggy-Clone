afd126323357a2c54b16360f2b11a94e754a8be4
pawan8893 <kumarpawansahh@gmail.com>
2024-11-21 19:10:48 +0530

    made the footer responsive according to different screen sizes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="bg-black">
-      <div className="pt-7 pb-11 mx-40">
-        <img className="h-[30px]" src="images/swiggy-foot.png" alt="swiggy-logo" />
-        <p className="text-gray-500">&copy;2023 Bundl Technologies Pvt. Ltd</p>
+      <div className="pt-7 pb-11 mx-4 sm:mx-10 md:mx-20 lg:mx-40 text-center sm:text-left">
+        <img
+          className="h-[20px] sm:h-[30px] mx-auto sm:mx-0"
+          src="images/swiggy-foot.png"
+          alt="swiggy-logo"
+        />
+        <p className="text-gray-500 mt-2 text-sm sm:text-base">
+          &copy;2023 Bundl Technologies Pvt. Ltd
+        </p>
       </div>
     </div>
   )
