08eabaf4885f3de6f80300cf5439156c991059ba
pawan8893 <kumarpawansahh@gmail.com>
2024-11-21 17:55:24 +0530

    adding the footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-black">
+      <div className="pt-7 pb-11 mx-40">
+        <img className="h-[30px]" src="images/swiggy-foot.png" alt="swiggy-logo" />
+        <p className="text-gray-500">&copy;2023 Bundl Technologies Pvt. Ltd</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
